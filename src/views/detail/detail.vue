b618f0058093410b077e2cde87b91e274ecbd987
许恩杰 <48144138+jieers@users.noreply.github.com>
2021-12-05 23:52:27 +0800

    'update'

--- src/views/detail/detail.vue
+++ src/views/detail/detail.vue
@@ -1,15 +1,38 @@
 <template>
   <div id="detail">
-    <detail-nav-bar></detail-nav-bar>
-    <detail-swiper :top-images="topImages"></detail-swiper>
+    <detail-nav-bar class="detail-nav" @titleClick="titleClick"></detail-nav-bar>
+    <scroll class="scroll-height" ref="scroll">
+      <detail-swiper :top-images="topImages"></detail-swiper>
+      <detail-base-info :goods="goods"></detail-base-info>
+      <detail-shop-info :shop="shop"></detail-shop-info>
+      <detail-goods-info :detail-info="detailInfo"></detail-goods-info>
+      <detail-param-info :param-info="paramsInfo" ref="params"></detail-param-info>
+      <detail-comment-info :comment-info="commentInfo" ref="comment"></detail-comment-info>
+      <goods-list :goods="recommends" ref="recommend"></goods-list>
+    </scroll>
+    <detail-bottom-bar @addToCart="addToCart"></detail-bottom-bar>
+    <back-top @backTop="backTop" class="back-top" v-show="showBackTop">
+      <img src="~assets/img/common/top.png" alt="">
+    </back-top>
   </div>
 </template>
 
 <script>
 import DetailNavBar from "./childComps/DetailNavBar"
 import DetailSwiper from "./childComps/DetailSwiper"
+import DetailBaseInfo from "@/views/detail/childComps/DetailBaseInfo";
+import DetailShopInfo from "@/views/detail/childComps/DetailShopInfo";
+import DetailGoodsInfo from "@/views/detail/childComps/DetailGoodsInfo";
+import DetailParamInfo from "@/views/detail/childComps/DetailParamInfo";
+import DetailCommentInfo from "@/views/detail/childComps/DetailCommentInfo";
+import DetailBottomBar from "@/views/detail/childComps/DetailBottomBar";
 
-import {getDetail} from "network/detail"
+import GoodsList from "@/components/content/goods/GoodsList";
+
+import Scroll from "components/common/scroll/Scroll";
+
+
+import {getDetail, getRecommend, Goods, Shop, GoodsParam} from "network/detail"
 
 export default {
   name: "detail",
@@ -17,23 +40,103 @@ export default {
     return {
       iid: null,
       topImages: [],
+      goods: {},
+      shop: {},
+      detailInfo: {},
+      paramsInfo: {},
+      commentInfo: {},
+      recommends: [],
+      themeTopYs: [],
     }
   },
   components: {
     DetailNavBar,
     DetailSwiper,
+    DetailBaseInfo,
+    DetailShopInfo,
+    Scroll,
+    DetailGoodsInfo,
+    DetailParamInfo,
+    DetailCommentInfo,
+    GoodsList,
+    DetailBottomBar,
+  },
+  methods: {
+    titleClick(index) {
+      this.$refs.scroll.scrollTo(0, -this.themeTopYs[index],200)
+    }
   },
   created() {
-      this.iid = this.$route.params.iid;
-      getDetail(this.iid).then(res => {
-        console.log(res);
-        this.topImages = res.result.itemInfo.topImages;
 
-      })
+    //保存iid
+    this.iid = this.$route.params.iid;
+    //根据iid请求详情数据
+    getDetail(this.iid).then(res => {
+      console.log(res);
+      const data = res.result
+      //获取商品顶部轮播图
+      this.topImages = data.itemInfo.topImages;
+
+      //获取商品信息
+      this.goods = new Goods(data.itemInfo, data.columns, data.shopInfo.services)
+
+      //获取店铺信息
+      this.shop = new Shop(data.shopInfo)
+
+      //获取商品详情数据
+      this.detailInfo = data.detailInfo
+
+      //获取商品参数
+      this.paramsInfo = new GoodsParam(data.itemParams.info, data.itemParams.rule)
+
+      //获取评论信息，注意，可能为空
+      if(data.rate.cRate !== 0) {
+        this.commentInfo = data.rate.list[0]
+      }
+
+      // this.$nextTick(() => {
+      //   this.themeTopYs = []
+      //   this.themeTopYs.push(0)
+      //   this.themeTopYs.push(this.$refs.params.$el.offsetTop)
+      //   this.themeTopYs.push(this.$refs.comment.$el.offsetTop)
+      //   this.themeTopYs.push(this.$refs.recommend.$el.offsetTop)
+      //
+      //   console.log(this.themeTopYs)
+      // })
+    })
+
+    //请求推荐数据
+    getRecommend().then(res => {
+      this.recommends = res.data.list
+    })
+
+
+  },
+
+
+  mounted() {
+
+
+  },
+  updated() {
+
   }
 }
 </script>
 
 <style scoped>
-
+  #detail {
+    position: relative;
+    z-index: 12;
+    background-color: #fff;
+    height: 100vh;
+  }
+  .detail-nav {
+    position: relative;
+    z-index: 12;
+    background-color: #fff;
+  }
+  .scroll-height {
+    height: calc(100% - 44px);
+  }
 </style>
